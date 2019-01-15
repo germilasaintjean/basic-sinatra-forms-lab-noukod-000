fc2ae545bbbcf8364c6cf304143cf5e5fb69645c
Saint-Jean Germila <germila@noukod.com>
2019-01-15 19:56:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,20 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get '/newteam' do
-   erb :newteam
-  end
-
-  post '/team' do
-         @team_name = @params["name"]
-         @coach = @params["coach"]
-         @point_guard = @params["pg"]
-         @shooting_guard = @params["sg"]
-         @small_forward = @params["sf"]
-         @power_forward= @params["pf"]
-         @center= @params["c"]
-
-         erb :team
+ get '/newteam' do
+     erb :newteam
  end
 
+ post '/team' do
+   @name = params["name"]
+   @coach = params["coach"]
+   @pg = params["pg"]
+   @sg = params["sg"]
+   @sf = params["sf"]
+   @pf = params["pf"]
+   @center = params["c"]
+
+   erb :team
+  end
 end
